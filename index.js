7a51d3814d1638a9d7a1d9b21634e10056fb169c
May-Hemade <may.hemade1993@gmail.com>
2021-11-11 17:20:33 +0200

    added team generation

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let waitingListStudentsContainerNode = document.getElementById(
   "waiting-list-students-container"
 )
-let teamsContainerNode = document.getElementById("teams-container")
+
+let studentInputNode = document.getElementById("student-name")
 
 const addStudent = function () {
-  let studentInputNode = document.getElementById("student-name")
   let studentName = studentInputNode.value
 
   if (studentName.length != 0) {
@@ -19,7 +19,24 @@ const addStudent = function () {
 }
 
 const createTeams = function () {
-  console.log("nyee")
+  let teamsContainerNode = document.getElementById("teams-container")
+
+  let numberTeamsInput = document.getElementById("number-teams")
+  console.log(numberTeamsInput.value)
+  let numberOfTeams = numberTeamsInput.value
+  for (let i = 1; i <= numberOfTeams; i++) {
+    let teamNode = document.createElement("div")
+    teamNode.classList.add("card", "col-4")
+
+    let headerNode = document.createElement("div")
+    headerNode.classList.add("card-header")
+
+    headerNode.innerText = `Team ${i}`
+
+    teamNode.appendChild(headerNode)
+
+    teamsContainerNode.appendChild(teamNode)
+  }
 }
 
 const onStudentEnterKey = function () {
@@ -35,3 +52,12 @@ const onStudentEnterKey = function () {
 window.onload = () => {
   onStudentEnterKey()
 }
+
+// <div class="card col-4">
+//               <div class="card-header">Featured</div>
+//               <ul class="list-group list-group-flush">
+//                 <li class="list-group-item">An item</li>
+//                 <li class="list-group-item">A second item</li>
+//                 <li class="list-group-item">A third item</li>
+//               </ul>
+//             </div>
